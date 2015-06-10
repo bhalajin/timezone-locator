a60a670f2cdb5845241eadaeb8b7036baf73ce33
Bhalaji <x0205349@ti.com>
2015-06-10 18:48:59 +0530

    server: setup: Add http module and create a http server
    Signed-off-by: Bhalaji <bhalaji@solitontech.com>

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -57,5 +58,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
