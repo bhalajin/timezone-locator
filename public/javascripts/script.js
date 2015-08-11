51e57361782b82e2f81f85b28a0b57a5a204a993
Bhalaji <x0205349@ti.com>
2015-08-11 21:02:48 +0530

    client: Add transition to move the search
    The search is moved to the top of the page and the location search is made
    display none
    
    Signed-off-by: Bhalaji <bhalaji@solitontech.com>

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,15 @@
 var searchLocation = function () {
   var location = $("#location").val();
   var locationURL = "https://maps.googleapis.com/maps/api/geocode/json?address="+location+"&key="+config.AUTHKEY;
+  $(".title-area").css({
+    'height': '0%',
+    'display': 'none'
+  });
+  $(".title-area-minimized").css({
+    'height': '10%',
+    'transition': 'height 0.5s'
+  });
+  $("#locationSearched").text(location);
   $.get(locationURL, function(data){
     if(data.status==='OK'){
       $("#latitude").text(data.results[0].geometry.location.lat);
